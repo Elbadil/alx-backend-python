081fea3b1747e0e85040e8185f98c4c571d0a05d
Elbadil <elbaliadil@gmail.com>
2024-02-03 14:42:42 +0100

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for client.py"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 
 
@@ -15,7 +15,7 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('requests.get')
     def test_org(self, org_name, mock_get_request):
-        """"""
+        """Tests for the org method using parameterized and patch"""
         mock_resp = Mock()
         orgs = GithubOrgClient(org_name)
         response_dict = {'name': 'Adil', 'age': 26}
@@ -26,6 +26,20 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_request.assert_called_with(
             GithubOrgClient.ORG_URL.format(org=org_name))
         self.assertEqual(get_json_data, response_dict)
+
+    def test_public_repos_url(self):
+        """Tests for the _public_repos_url property"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {
+                'name': 'google',
+                'repos_url': 'https://api.github.com/orgs/google/repos_url'
+                }
+            orgs = GithubOrgClient('google')
+            self.assertEqual(orgs.org, mock_org.return_value)
+            mock_org.assert_called_once()
+            self.assertEqual(orgs._public_repos_url,
+                             'https://api.github.com/orgs/google/repos_url')
 
 
 if __name__ == '__main__':
