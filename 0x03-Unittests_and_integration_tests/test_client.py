a8d34c583b2746e9c1a15a9db17f5072d906d903
Elbadil <elbaliadil@gmail.com>
2024-02-03 19:51:50 +0100

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Unit tests for client.py"""
+"""Unit tests for GithubOrgClient class"""
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock, PropertyMock
@@ -33,13 +33,38 @@ class TestGithubOrgClient(unittest.TestCase):
                    new_callable=PropertyMock) as mock_org:
             mock_org.return_value = {
                 'name': 'google',
-                'repos_url': 'https://api.github.com/orgs/google/repos_url'
-                }
+                'repos_url': 'https://api.github.com/orgs/google/repos'
+            }
             orgs = GithubOrgClient('google')
             self.assertEqual(orgs.org, mock_org.return_value)
             mock_org.assert_called_once()
             self.assertEqual(orgs._public_repos_url,
-                             'https://api.github.com/orgs/google/repos_url')
+                             'https://api.github.com/orgs/google/repos')
+
+    @patch('requests.get')
+    def test_public_repos(self, mock_get_request):
+        """Tests for the public_repos method"""
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_property:
+            link = 'https://api.github.com/orgs/google/repos'
+            mock_property.return_value = link
+            mock_resp = Mock()
+            orgs = GithubOrgClient('google')
+            response_list = [
+                {'id': 1936771, 'name': 'truth'},
+                {'id': 1936772, 'name': 'autoparse'},
+                {'id': 1936773, 'name': 'anvil-build'},
+            ]
+            mock_resp.json.return_value = response_list
+            mock_get_request.return_value = mock_resp
+            get_json_resp = orgs.repos_payload
+            self.assertEqual(get_json_resp, response_list)
+            mock_get_request.assert_called_once()
+
+            repos_list = ['truth', 'autoparse', 'anvil-build']
+            get_json_names = orgs.public_repos()
+            mock_property.assert_called_once()
+            self.assertEqual(repos_list, get_json_names)
 
 
 if __name__ == '__main__':
